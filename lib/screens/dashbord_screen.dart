a14cd03fb56d7d3564285cb21050035c299947de
Venkatesh <74035874+NVenkateshRaj@users.noreply.github.com>
2024-06-28 11:03:11 -0700

    Update dashbord_screen.dart
    search field example

--- lib/screens/dashbord_screen.dart
+++ lib/screens/dashbord_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:network_issue_handle/constants/colors.dart';
-import 'package:network_issue_handle/constants/images.dart';
 import 'package:network_issue_handle/model/bread_crumb_widget_model.dart';
-import 'package:network_issue_handle/widgets/bread_crumb_widget.dart';
-import 'package:network_issue_handle/widgets/button_widget.dart';
 import 'package:network_issue_handle/widgets/common_appbar.dart';
 import 'package:network_issue_handle/widgets/common_text_field.dart';
 
@@ -24,6 +20,18 @@ class _DashboardState extends State<DashboardScreen>{
   TextEditingController phoneController = TextEditingController();
   TextEditingController decimalController = TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  List<String> searchList = ["Abc","DEF","Ghi","jkl"];
+  final List<String> items = [
+    'Item1',
+    'Item2',
+    'Item3',
+    'Item4',
+    'Item5',
+    'Item6',
+    'Item7',
+    'Item8',
+  ];
+  String? selectedValue;
 
   void _submit() {
     final isValid = formKey.currentState!.validate();
@@ -44,11 +52,29 @@ class _DashboardState extends State<DashboardScreen>{
 
   @override
   Widget build(BuildContext context) {
-
     return  Scaffold(
       appBar: const CommonAppbar(),
-      body: BreadCrumbWidget(
-        breadCrumbList: breadCrumbWidgetList,
+      body: Column(
+        children: [
+          CommonTextField(
+              textFieldTitle: "Enter Name",
+              controller: nameController,
+              hintText: "Enter Name",
+              onChanged: (val){},
+              onSubmitted: (val){}
+          ),
+          SizedBox(height: 20.h,),
+          CommonTextField(
+              textFieldTitle: "Enter Name",
+              controller: nameController,
+              hintText: "Enter Name",
+              onChanged: (val){},
+              onSubmitted: (val){},
+            isSearchField: true,
+            suggestions: searchList,
+          ),
+          SizedBox(height: 20.h,),
+        ],
       ),
     );
   }
